ae2cdfdfab23f475eb70c1dc25e5c65661ac6e67
Andrew Petro <andrew.petro@hey.com>
2024-09-14 18:18:54 -0500

    factor out combat into a function

---
+++ docs/development/blogs/008-refactor-combat/README.md
@@ -0,0 +1,10 @@
+# Refactoring combat
+
+So, combat should be a function.
+
+Navigation:
+
++ [Documentation home](../../../README.md)
++ [Development documentation home](../../README.md)
++ [Blogs home](../README.md)
++ [Previous post](../006-escape/README.md): Escaping the spider nest
--- src/main.rs
+++ src/main.rs
@@ -34,49 +34,19 @@ fn main() {
 
         match action {
             "HUNT" => {
-
-
-                let spider = level_one_spider();
-
-                println!("You have encountered a {}.", spider.name);
-
-                'spider: loop {
-                    if d20() >= player_character.evade {
-                        player_character.health = player_character.health - 1;
-
-                        println!(
-                            "The {} {} you. You have {} health remaining.", spider.name, spider.attack_verb, player_character.health
-                        );
-
-                        if player_character.health < 1 {
-                            println!("You have succumbed to your wounds.");
-                            break 'main;
-                        }
-                    }
-
-                    if d12() + d12() >= spider.hit_difficulty {
-                        println!("You have defeated the {}.", spider.name);
-
-                        println!(
-                            "The {} evaporates in a cloud of foul-smelling rainbow smoke.", spider.name
-                        );
-                        println!("Incredibly, when the smoke clears it reveals an iron spirit coin where once there was a monster.");
-                        character_treasure = character_treasure + 1;
-                        println!(
-                        "You take the coin. You now have {character_treasure} lesser spirit coins."
-                    );
-                        println!();
-                        break 'spider;
-                    } else {
-                        println!("You swing your rusty sword at the {}, but miss.", spider.name)
-                    }
+                let monster = level_one_spider();
+                let combat_result = combat_with_monster(player_character, monster);
+                player_character = combat_result.player_character;
+                if player_character.health < 1 {
+                    break 'main;
                 }
-            },
+                character_treasure = character_treasure + combat_result.lesser_spirit_coins;
+            }
 
             "LEAVE" => {
-              println!("Discretion is the better part of valor. You leave the nest.");
-              break 'main;
-            },
+                println!("Discretion is the better part of valor. You leave the nest.");
+                break 'main;
+            }
 
             "INVENTORY" => {
                 println!("You are wielding a rusty sword in your dominant hand.");
@@ -176,4 +146,46 @@ fn level_one_spider() -> Monster {
     }
 }
 
+struct CombatResult {
+    player_character: PlayerCharacter,
+    lesser_spirit_coins: usize
+}
+
+fn combat_with_monster(mut player_character: PlayerCharacter, monster: Monster) -> CombatResult {
+    println!("You have encountered a {}.", monster.name);
+
+    'monster: loop {
+        if d20() >= player_character.evade {
+            player_character.health = player_character.health - 1;
+
+            println!(
+                "The {} {} you. You have {} health remaining.",
+                monster.name, monster.attack_verb, player_character.health
+            );
+
+            if player_character.health < 1 {
+                println!("You have succumbed to your wounds.");
+                break 'monster;
+            }
+        }
+
+        if d12() + d12() >= monster.hit_difficulty {
+            println!("You have defeated the {}.", monster.name);
+
+            println!(
+                "The {} evaporates in a cloud of foul-smelling rainbow smoke.",
+                monster.name
+            );
+            println!("When the smoke dissipates, a lesser spirit coin remains, which you grab.");
+            println!();
+            break 'monster;
+        } else {
+            println!(
+                "You swing your rusty sword at the {}, but miss.",
+                monster.name
+            )
+        }
+    }
+
+    CombatResult { player_character, lesser_spirit_coins: 1 }
 }
