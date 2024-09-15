9aa749f2fba8aa78f409c82c9db8773e00eae8a1
Andrew Petro <andrew.petro@hey.com>
2024-09-15 11:34:49 -0500

    move module dice to file dice.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 
+pub mod dice;
 pub mod dungeon;
 
 use crate::dungeon::explore_dungeon;
@@ -84,22 +85,6 @@ fn main() {
     );
 
     println!("The end.");
-}
-
-pub mod dice {
-    use rand::Rng;
-
-    fn roll_die(dice_size: usize) -> usize {
-        rand::thread_rng().gen_range(1..=dice_size)
-    }
-
-    pub fn d12() -> usize {
-        roll_die(12)
-    }
-
-    pub fn d20() -> usize {
-        roll_die(20)
-    }
 }
 
 struct PlayerCharacter {
