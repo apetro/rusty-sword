42159830ff3107200971c79039f45b68e420c404
Andrew Petro <andrew.petro@hey.com>
2024-09-14 20:31:35 -0500

    add troll bridge

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,50 @@ fn main() {
 
     println!("Greetings, {}.", player_character.name);
 
-    let nest_of_spiders = Dungeon { name: "nest of spiders".to_string(), flavor_text: "A dark nest of scary spiders. It's a tough job clearing them out, but someone has got to do it.".to_string(), monsters: spiders()};
 
-    let dungeon_result = explore_dungeon(player_character, nest_of_spiders, inventory);
+    loop {
 
-    player_character = dungeon_result.player_character;
-    inventory = dungeon_result.inventory;
+        let mut action = String::new();
+        println!();
+        println!(
+            "Would you like to clear out the SPIDERS nest, confront the BRIDGE troll, view your INVENTORY, or RETIRE from adventuring?"
+        );
+        io::stdin()
+            .read_line(&mut action)
+            .expect("Failed to read line");
+        let action = action.trim();
+
+        match action {
+            "SPIDERS" => {
+                let nest_of_spiders = Dungeon { name: "nest of spiders".to_string(), flavor_text: "A dark nest of scary spiders. It's a tough job clearing them out, but someone has got to do it.".to_string(), monsters: spiders()};
+
+                let dungeon_result = explore_dungeon(player_character, nest_of_spiders, inventory);
+
+                player_character = dungeon_result.player_character;
+                inventory = dungeon_result.inventory;
+            }
+            "BRIDGE" => {
+                let troll_bridge: Dungeon = Dungeon { name: "troll bridge".to_string(), flavor_text: "A bridge under which lives a grumpy troll".to_string(), monsters: vec![Monster {name: "Bridge Troll".to_string(), attack_verb: "whomps".to_string(), hit_difficulty: 20}]};
+                let dungeon_result = explore_dungeon(player_character, troll_bridge, inventory);
+
+                player_character = dungeon_result.player_character;
+                inventory = dungeon_result.inventory;
+            }
+
+            "INVENTORY" => print_inventory(&inventory),
+
+            "RETIRE" => break,
+
+            _ => println!("{} wasn't a choice?", action),
+        }
+
+        if player_character.health < 1 {
+            break
+        }
+
+    }
+
+
 
     println!(
         "{} ended the game with {} lesser spirit coins.",
